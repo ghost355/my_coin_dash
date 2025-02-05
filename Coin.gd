86234f779f8336cc9a7ca710155b67ec5b89388d
P. Pavlov <kx355@mail.ru>
2025-02-05 20:02:29 +0300

    add class node Coin

---
+++ Coin.gd
@@ -0,0 +1,21 @@
+class_name Coin
+extends Area2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var timer = $AnimationTimer
+
+
+func _ready() -> void:
+	timer.timeout.connect(_on_timer_timeout)
+	random_timer(0.5, 4.5)
+
+
+func _on_timer_timeout() -> void:
+	animated_sprite.play()
+	random_timer(0.5, 4.5)
+
+
+func random_timer(from: float, to: float) -> void:
+	var time = randf_range(from, to)
+	timer.wait_time = time
+	timer.start()
